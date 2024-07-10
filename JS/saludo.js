2a7170be30efd92436ae91b89a13f1704ca8fa5f
4peacenfreedom <4peacenfreedom@gmail.com>
2024-07-09 19:51:05 -0600

    ft-practica-jul9

---
+++ JS/saludo.js
@@ -0,0 +1,14 @@
+//document.getElementById("resultado").textContent = "Hola Mundo";
+//document.getElementById("resultado").style.backgroundColor = "blue";
+
+let DivResultado = document.getElementById("resultado");
+console.log(DivResultado);
+
+DivResultado.textContent = "Hola Mundo";
+DivResultado.style.backgroundColor = "red";
+
+let pTag = document.getElementById("test1");
+console.log(pTag);
+
+pTag.textContent = "Esto es un parrafo";
+pTag.style.backgroundColor = "green";

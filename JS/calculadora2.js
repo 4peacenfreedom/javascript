0277b87f1b23104783805255d0d15454190237a4
4peacenfreedom <4peacenfreedom@gmail.com>
2024-07-11 19:26:33 -0600

    ft-update de la calculadora

---
+++ JS/calculadora2.js
@@ -0,0 +1,11 @@
+console.log("Funcionando Calculadora");
+
+function calcular() {
+  event.preventDefault();
+  const Num1 = document.getElementById("Num1");
+  const Num2 = document.getElementById("Num2");
+  const Resultado = document.getElementById("Resultado");
+
+  let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
+  Resultado.textContent = `El resultado es ${calculo}`;
+}

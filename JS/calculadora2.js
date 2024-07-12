bb83e66ef9643cd2cf0eba89221462515eae0d0b
4peacenfreedom <4peacenfreedom@gmail.com>
2024-07-11 21:36:37 -0600

    ft-eventos

--- JS/calculadora2.js
+++ JS/calculadora2.js
@@ -1,21 +1,48 @@
 console.log("Funcionando Calculadora");
 
-function calcular() {
+// function calcular() {
+//   event.preventDefault();
+//   const Num1 = document.getElementById("Num1");
+//   const Num2 = document.getElementById("Num2");
+//   const Resultado = document.getElementById("Resultado");
+
+//   let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
+//   Resultado.textContent = `El resultado es ${calculo}`;
+// }
+
+// function restar() {
+//   event.preventDefault();
+//   const Num1 = document.getElementById("Num1");
+//   const Num2 = document.getElementById("Num2");
+//   const Resultado = document.getElementById("Resultado");
+
+//   let calculo = parseFloat(Num1.value) - parseFloat(Num2.value);
+//   Resultado.textContent = `El resultado es ${calculo}`;
+// }
+
+function calcular2(event) {
   event.preventDefault();
-  const Num1 = document.getElementById("Num1");
-  const Num2 = document.getElementById("Num2");
   const Resultado = document.getElementById("Resultado");
-
-  let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
-  Resultado.textContent = `El resultado es ${calculo}`;
-}
-
-function restar() {
-  event.preventDefault();
-  const Num1 = document.getElementById("Num1");
-  const Num2 = document.getElementById("Num2");
-  const Resultado = document.getElementById("Resultado");
-
-  let calculo = parseFloat(Num1.value) - parseFloat(Num2.value);
-  Resultado.textContent = `El resultado es ${calculo}`;
+  const Num1 = parseFloat(document.getElementById("Num1").value);
+  const Num2 = parseFloat(document.getElementById("Num2").value);
+  const operaciones = document.getElementById("operaciones").value;
+  let calculo;
+  switch (operaciones) {
+    case "+":
+      calculo = Num1 + Num2;
+      break;
+    case "-":
+      calculo = Num1 - Num2;
+      break;
+    case "*":
+      calculo = Num1 * Num2;
+      break;
+    case "/":
+      calculo = Num1 / Num2;
+      break;
+    default:
+      calculo = "Operacion no valida";
+      break;
+  }
+  Resultado.textContent = `El Resultado es : ${calculo}`;
 }

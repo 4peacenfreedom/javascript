ab6f327c6a7ba0ab8f0a7321027e90f642ef1c8c
Marcelo89890 <marcelo.morap19@gmail.com>
2024-07-11 20:07:35 -0600

    calculadora Switch

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -1,12 +1,60 @@
 console.log("Funcionando Calculadora")
+//Global
 
-function Calcular(event) {
+
+
+function Calculadora2(event) {
     event.preventDefault();
-    const Num1 = document.getElementById("Num1").value;
-    const Num2 = document.getElementById("Num2").value;
     const Resultado = document.getElementById("Resultado");
+    const Num1 =parseFloat(document.getElementById("Num1").value);
+    const Num2 =parseFloat(document.getElementById("Num2").value);
+    const operaciones = document.getElementById("operaciones").value;
+    let calculo;
+    switch (operaciones) {
+        case "+":
+            calculo = Num1 + Num2
+            
+            break;
+        case "-":
+                calculo = Num1 - Num2
+            break;
+        case "*":
+                calculo = Num1 * Num2
+            break;
+        case "/":
+                calculo = Num1 / Num2
+            break;
+        default:
+                calculo = "Operacion no valida"
+            break;
+    }
+    Resultado.textContent = `El Resultado es : ${calculo} `
 
-    let calculo = Num1 + Num2 ;
-    Resultado.textContent = `El resultado es ${calculo}`
+
+   
 
 }
+
+
+// function Sumar(event) {
+//     event.preventDefault();
+
+ 
+
+//     //parsefloat convierte string a decimal
+//     //parseint convierte string a int
+
+//     let calculo = parseFloat(Num1.value) +  parseFloat(Num2.value) ;
+//     Resultado.textContent = `El resultado es ${calculo}`
+    
+// }
+
+// function Restar(event) {
+    
+
+//     //parsefloat convierte string a decimal
+//     //parseint convierte string a int
+
+//     let calculo = parseFloat(Num1.value) - parseFloat(Num2.value) ;
+//     Resultado.textContent = `El resultado es ${calculo}`
+// }

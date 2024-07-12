d968d8f5ae379d55f8cfaeb59cadda72922f68cd
4peacenfreedom <4peacenfreedom@gmail.com>
2024-07-11 19:38:00 -0600

    ft-resta

--- JS/calculadora2.js
+++ JS/calculadora2.js
@@ -9,3 +9,13 @@ function calcular() {
   let calculo = parseFloat(Num1.value) + parseFloat(Num2.value);
   Resultado.textContent = `El resultado es ${calculo}`;
 }
+
+function restar() {
+  event.preventDefault();
+  const Num1 = document.getElementById("Num1");
+  const Num2 = document.getElementById("Num2");
+  const Resultado = document.getElementById("Resultado");
+
+  let calculo = parseFloat(Num1.value) - parseFloat(Num2.value);
+  Resultado.textContent = `El resultado es ${calculo}`;
+}
